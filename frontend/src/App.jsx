5cb5a4b1d5d1920b6b21bc933013ff10cad20af9
ahing1 <ahing1@stevens.edu>
2025-01-23 21:56:31 -0500

    added video feed NEEDS TESTING

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,13 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { checkHealth } from './services/api'
+import React from "react";
+import VideoFeed from "./components/VideoFeed";
 
 function App() {
-  const [healthStatus, setHealthStatus] = useState('')
-
-  useEffect(() => {
-    const fetchHealth = async () => {
-      const status = await checkHealth();
-      setHealthStatus(status ? status.status : 'Failed');
-    }
-    fetchHealth();
-  }, [])
-
   return (
     <div className="App">
       <h1>Smart Personal Trainer</h1>
-      <p>Backend Health: {healthStatus}</p>
+      <VideoFeed />
     </div>
   );
 }
 
-export default App
+export default App;
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "http://127.0.0.1:5000" });
+export const API = axios.create({ baseURL: "http://127.0.0.1:5000" });
 
 export const checkHealth = async () => {
     try{
